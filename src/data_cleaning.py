9f17af898a547b599c615fc9005391bae5a7c8d1
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-04-20 20:58:58 +0530

    :fire: data cleaning process finished

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -17,7 +17,7 @@ class DataStrategy(ABC):
         """
         Initialize the data cleaning object.
 
-        :param data: The data to clean.
+        data: The data to clean.
         """
         if isinstance(data, pd.DataFrame):
             self.data = data
@@ -27,7 +27,7 @@ class DataStrategy(ABC):
             raise ValueError("data must be a pandas DataFrame or a path to a CSV file.")
 
     @abstractmethod
-    def handle_data(self) ->Union[pd.DataFrame,pd.Series]:
+    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
         """
         Clean the data.
 
@@ -36,11 +36,11 @@ class DataStrategy(ABC):
         pass
 
 
-
 class DataPreProcessStrategy(DataStrategy):
     """
     Class for data cleaning using preprocessing techniques.
     """
+
     def __init__(self, data: Union[pd.DataFrame, str]):
         """
         Initialize the data cleaning object.
@@ -55,20 +55,128 @@ class DataPreProcessStrategy(DataStrategy):
 
         :return: The cleaned data.
         """
-        try :
+        try:
             logging.info("Cleaning the data using preprocessing techniques.")
-            self.data = self.data.drop([
-                "order_approved_at",
-                "order_delivered_carrier_date",
-                "order_delivered_customer_date",
-                "order_estimated_delivery_date",
-                "order_purchase_timestamp",
-            ], axis=1) # Drop unnecessary columns
 
+            self.data = self.data.drop(
+                [
+                    "order_approved_at",
+                    "order_delivered_carrier_date",
+                    "order_delivered_customer_date",
+                    "order_estimated_delivery_date",
+                    "order_purchase_timestamp",
+                    "order_status",
+                    "customer_zip_code_prefix",
+                    "order_item_id",
+                ],
+                axis=1,
+            )  # Drop unnecessary columns
+
+            # fill up the null | missing values  (refer to the EDA)
+
+            self.data["product_weight_g"].fillna(
+                self.data["product_weight_g"].mean(), inplace=True
+            )
+            self.data["product_length_cm"].fillna(
+                self.data["product_length_cm"].mean(), inplace=True
+            )
+            self.data["product_height_cm"].fillna(
+                self.data["product_height_cm"].mean(), inplace=True
+            )
+            self.data["product_width_cm"].fillna(
+                self.data["product_width_cm"].mean(), inplace=True
+            )
+            self.data["review_comment_message"].fillna("No comment", inplace=True)
+
+            # pick the np.number data type columns
+            self.data = self.data.select_dtypes(include=[np.number])
 
             return self.data
         except Exception as e:
             logging.error(f"An error occurred while cleaning the data: {e}")
             raise e
-    
-    
+
+
+class DataSplitStrategy(DataStrategy):
+    """
+    Divide the data into training and testing sets.
+    """
+
+    def __init__(self, data: Union[pd.DataFrame, str]):
+        """
+        Initialize the data cleaning object.
+
+        :param data: The data to clean.
+        """
+        super().__init__(data)
+
+    def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
+
+        """
+        Clean the data using splitting techniques.
+
+        :return: The cleaned data.
+        """
+        try:
+            logging.info("Cleaning the data using splitting techniques.")
+
+            # Split the data into features and target
+            X = self.data.drop("review_score", axis=1)  # Features
+            y = self.data["review_score"]  # Target variable
+
+            # Split the data into training and testing sets (80% training, 20% testing)
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.2, random_state=42
+            )
+
+            return X_train, X_test, y_train, y_test
+        except Exception as e:
+            logging.error(f"An error occurred while cleaning the data: {e}")
+            raise e
+
+
+class DataCleaninig:
+    """
+    Class for clearning data using a strategy then divide the data into training and testing sets.
+    """
+
+    def __init__(self, data: pd.DataFrame, strategy: DataStrategy):
+        """
+        Initialize the data cleaner object.
+
+        data: The data to clean.
+        strategy: The strategy to use for cleaning the data.
+        """
+        self.data = data
+        self.strategy = strategy
+
+    def clean_data(self) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Clean the data using the specified strategy.
+
+        :return: The cleaned data.
+        """
+        try:
+            return self.strategy.handle_data()
+        except Exception as e:
+            logging.error(f"An error occurred while cleaning the data: {e}")
+            raise e
+
+
+if __name__ == "__main__":
+    data = pd.read_csv(
+        "/Volumes/Project-2/programming/machine_deep_learning/projects/customer_satisfaction/data/olist_customers_dataset.csv"
+    )
+    data_cleaner = DataCleaninig(data, DataPreProcessStrategy(data))
+    cleaned_data = data_cleaner.clean_data()
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = DataSplitStrategy(cleaned_data).handle_data()
+
+    print("Data cleaning and splitting complete.")
+
+    print("Size of training set:", X_train.shape)
+    print("Size of testing set:", X_test.shape)
+    print("Size of training labels:", y_train.shape)
+    print("Size of testing labels:", y_test.shape)
+
--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,10 +1,16 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.data_cleaning import DataPreProcessStrategy,DataCleaninig,DataSplitStrategy
 
+from typing import Annotated, Tuple
 
 @step
-def clean_data(data: pd.DataFrame) -> None:
+def clean_data(data: pd.DataFrame) -> Tuple[
+             Annotated[pd.DataFrame, 'X_train'],
+          Annotated[pd.DataFrame, 'X_test'],
+          Annotated[pd.DataFrame, 'y_train'],
+          Annotated[pd.DataFrame, 'y_test']]:
     """
     Cleaning the data.
 
@@ -12,9 +18,23 @@ def clean_data(data: pd.DataFrame) -> None:
         data (pd.DataFrame): data to be cleaned
 
     Returns:
-        pd.DataFrame: cleaned data
+         Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]: cleaned data
     """
-    logging.info("Cleaning data")
-    # Dropping rows with missing values
+    try:
+        logging.info("Cleaning data")
+        # Data cleaning
+        process_strategy = DataPreProcessStrategy(data)
+        data_cleaning=DataCleaninig(data,process_strategy)
+        processed_data= data_cleaning.handle_data()
+        # Splitting the data
+        split_strategy=DataSplitStrategy(processed_data)
+
+        X_train ,X_test,y_train,y_test=split_strategy.handle_data()
+
+        logging.info("Data cleaning complete !")
+
+        return X_train ,X_test,y_train,y_test
     
-    # return data.dropna()
+    except Exception as e:
+        logging.error(f"An error occurred while cleaning the data: {e}")
+        raise e
