f1401545e8db991603476d7ced55e58ccd6d9934
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2024-04-20 21:25:30 +0530

    Add model development module and classes

---
+++ src/model_devlopment.py
@@ -0,0 +1,85 @@
+import logging
+from abc import ABC, abstractmethod
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
+
+class Model(ABC):
+    """
+    Abstract class for model development.
+    """
+
+    @abstractmethod
+    def train(self,x_train,y_train):
+        """
+        Develop the model.
+
+        Args:
+            x_train (pd.series):  training data 
+            y_train (pd.serires):  labels
+
+        Returns:
+            model: The developed model.
+        """
+        pass
+
+
+
+class LinearRegression(Model):
+    """
+    Class for linear regression model development.
+    """
+
+    @abstractmethod
+    def train(self,x_train,y_train,**kwargs):
+        """
+        Develop the linear regression model.
+
+        Args:
+            x_train (pd.series):  training data 
+            y_train (pd.serires):  labels
+
+        Returns:
+            model: The developed model.
+        """
+        try:
+            logging.info("Linear regression model developed.")
+            reg=LinearRegression(**kwargs)
+            reg.fit(x_train,y_train)
+            return reg
+        except Exception as e:
+            logging.error(f"An error occurred while developing the linear regression model: {e}")
+            raise e
+        
+
+
+class RandomForest(Model):
+    """
+    Class for Random Forest model development.
+    """
+
+    @abstractmethod
+    def train(self,x_train,y_train,**kwargs):
+        """
+        Develop the Random Forest model.
+
+        Args:
+            x_train (pd.series):  training data 
+            y_train (pd.serires):  labels
+
+        Returns:
+            model: The developed model.
+        """
+        try:
+            logging.info("Random Forest model developed.")
+            reg=RandomForestRegressor(**kwargs)
+            reg.fit(x_train,y_train)
+            return reg
+        except Exception as e:
+            logging.error(f"An error occurred while developing the Random Forest model: {e}")
+            raise e
+        
+        
+
+
